88a48c184d5e76b68a5f23c14841b3a238ebff44
Emre YILMAZ <emre.yilmaz@hotmail.co.uk>
2025-06-10 17:49:00 +0300

    çoklu seçim ekranı

--- purchase/form/depodan_teslim.js
+++ purchase/form/depodan_teslim.js
@@ -420,7 +420,7 @@ uniqueProducts.forEach(productId => {
                 ? `<div class="tooltip"><span class="price-original">${product.PRICE_OTHER.toFixed(2)} ${product.OTHER_MONEY}</span><span class="tooltiptext">İskonto: ${product.DISCOUNT_1}%</span></div>`
                 : `<div><strong>${product.PRICE_OTHER.toFixed(2)} ${product.OTHER_MONEY}</strong></div>`;
 
-            if(product.netPrice > 0) {
+            if(product.NET_PRICE > 0) {
             let netPriceHtml = `<div class=\"net-price\">Net: ${product.NET_PRICE.toFixed(2)} TL <div><strong>${cmqconvertedPrice.toFixed(2)} ${DEMAND_MONEY}</strong></div></div>`;
             if (product.NET_PRICE === lowestNetPrice) {
                 netPriceHtml = `<div class=\"net-price\">⭐ Net: ${product.NET_PRICE.toFixed(2)} TL <div><strong>${cmqconvertedPrice.toFixed(2)} ${DEMAND_MONEY}</strong></div></div>`;
