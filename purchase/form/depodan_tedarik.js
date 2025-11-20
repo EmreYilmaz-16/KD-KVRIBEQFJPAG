a938b8fc48f6517ed6e30b7b4653288b497b7581
Emre YILMAZ <emre.yilmaz@hotmail.co.uk>
2025-11-20 09:44:11 +0300

    mal kabul ekranı

--- purchase/form/depodan_tedarik.js
+++ purchase/form/depodan_tedarik.js
@@ -168,7 +168,7 @@ data.forEach(supplier => {
 const uniqueProducts = Array.from(productSet);
 
 const headerRow = document.createElement('tr');
-headerRow.innerHTML = `<th class="sticky-header bg-success text-white">&Uuml;r&uuml;n</th><th class="sticky-header bg-success text-white">Urun Kodu</th><th class="sticky-header bg-success text-white">Oem No</th>`;
+headerRow.innerHTML = `<th class="sticky-header bg-success text-white">&Uuml;r&uuml;n</th><th class="sticky-header bg-success text-white">Urun Kodu</th><th class="sticky-header bg-success text-white">Marka</th><th class="sticky-header bg-success text-white">Oem No</th>`;
 headerRow.innerHTML += `
   <th class="sticky-header bg-info text-white">
     Marj (%)<br>
